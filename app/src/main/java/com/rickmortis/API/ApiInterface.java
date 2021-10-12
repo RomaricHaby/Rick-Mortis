e9b00c78bf6fde47510b39b6106430a84a49beca
haby <haby.romaric@gmail.com>
2021-10-12 23:28:13 +0200

    Add model + retrofit + up gradle

---
+++ app/src/main/java/com/rickmortis/API/ApiInterface.java
@@ -0,0 +1,27 @@
+package com.rickmortis.API;
+
+import com.rickmortis.Model.Character.DataCharacterApi;
+import com.rickmortis.Model.Episode.DataEpisodeApi;
+import com.rickmortis.Model.Location.DataLocationApi;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    @Headers("Content-Type: application/json")
+
+    @GET("location/")
+    Call<DataLocationApi> getLocation(@Query("page") int page);
+
+    @GET("episode/")
+    Call<DataEpisodeApi> getEpisode(@Query("page") int page);
+
+    @GET("character/")
+    Call<DataCharacterApi> getCharacter(@Query("page") int page);
+
+    @GET("character/")
+    Call<DataCharacterApi> getCharacterFilter(@Query("name") String name);
+}
---
+++ app/src/main/java/com/rickmortis/Model/Character/DataCharacterApi.java
@@ -0,0 +1,37 @@
+package com.rickmortis.Model.Character;
+
+import com.example.tp_iem.Modele.Info;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class DataCharacterApi {
+
+
+    @SerializedName("info")
+    @Expose
+    private Info info;
+    @SerializedName("results")
+    @Expose
+    private List<Character> characters = null;
+
+    public Info getInfo() {
+        return info;
+    }
+
+    public void setInfo(Info info) {
+        this.info = info;
+    }
+
+    public List<Character> getCharacters() {
+        return characters;
+    }
+
+    public void setCharacters(List<Character> characters) {
+        this.characters = characters;
+    }
+
+}
+
+
