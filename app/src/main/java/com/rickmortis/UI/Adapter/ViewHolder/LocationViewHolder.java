a12459ce62968c3023663c36c4deee59813e561d
haby <haby.romaric@gmail.com>
2021-10-13 19:57:19 +0200

    Add frag location

--- app/src/main/java/com/rickmortis/UI/Adapter/ViewHolder/LocationViewHolder.java
+++ app/src/main/java/com/rickmortis/UI/Adapter/ViewHolder/LocationViewHolder.java
@@ -1,4 +1,33 @@
 package com.rickmortis.UI.Adapter.ViewHolder;
 
-public class LocationViewHolder {
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.rickmortis.Model.Location.Location;
+import com.rickmortis.R;
+
+public class LocationViewHolder extends RecyclerView.ViewHolder{
+    private final TextView name;
+    private final TextView type;
+    private final TextView dimension;
+
+    public LocationViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        name = itemView.findViewById(R.id.name_location_tv);
+        type = itemView.findViewById(R.id.name_type_tv);
+        dimension = itemView.findViewById(R.id.dimension_location_tv);
+    }
+
+
+
+    public void updateLocation(Location location){
+        name.setText(location.getName());
+        type.setText(location.getType());
+        dimension.setText(location.getDimension());
+    }
 }
--- app/src/main/java/com/rickmortis/UI/Fragment/LocationFragment.java
+++ app/src/main/java/com/rickmortis/UI/Fragment/LocationFragment.java
@@ -1,66 +1,113 @@
 package com.rickmortis.UI.Fragment;
 
+
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.rickmortis.API.ApiClient;
+import com.rickmortis.API.ApiInterface;
+import com.rickmortis.Model.Location.DataLocationApi;
 import com.rickmortis.R;
+import com.rickmortis.UI.Adapter.LocationAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LocationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class LocationFragment extends Fragment {
+    private String TAG ="LocationFragment";
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    //API
+    private ApiInterface apiService;
+    private LocationAdapter locationAdapter;
+    private int page = 1;
+    private RecyclerView recyclerView;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LocationFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LocalisationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LocationFragment newInstance(String param1, String param2) {
-        LocationFragment fragment = new LocationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    public LocationFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_localisation, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_localisation, container, false);
+
+        initUI(view);
+
+        // init API
+        apiService = ApiClient.getClient().create(ApiInterface.class);
+
+        //Call data API
+        getLocationCallback();
+
+        //set scroll with page
+        setRecyclerViewScroll();
+
+        return view;
     }
+
+    private void initUI(View view) {
+        this.recyclerView = view.findViewById(R.id.recyclerViewLocation);
+    }
+    
+    //Init recycler view
+    private void setRecyclerViewLocation(DataLocationApi dataLocationApi) {
+        // Create adapter passing in the sample user data
+        locationAdapter = new LocationAdapter(dataLocationApi.getLocations());
+        // Attach the adapter to the recyclerview to populate items
+        this.recyclerView.setAdapter(locationAdapter);
+        this.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    //Scroll view
+    private void setRecyclerViewScroll(){
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (!recyclerView.canScrollVertically(1)) {
+                   getLocationCallback();
+
+                }
+            }
+        });
+    }
+
+    //Request API
+    private void getLocationCallback() {
+        this.apiService.getLocation(page).enqueue(new Callback<DataLocationApi>() {
+            @Override
+            public void onResponse(Call<DataLocationApi> call, Response<DataLocationApi> response) {
+                //Init recycler view
+                if (page == 1){
+                    if (response.body() != null) {
+                        setRecyclerViewLocation(response.body());
+                    }
+                }
+                else{
+                    if (response.body() != null && page != response.body().getInfoLocation().getPages() + 1) {
+                        locationAdapter.addData(response.body().getLocations());
+                        locationAdapter.notifyDataSetChanged();
+                    }
+                }
+                page++;
+            }
+
+            @Override
+            public void onFailure(Call<DataLocationApi> call, Throwable t) {
+                Log.e(TAG, "Throwable" + t);
+            }
+        });
+    }
+
 }
